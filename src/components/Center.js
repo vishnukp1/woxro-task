70f46df8dda40b99528b3b1c74ea34660b66bf7c
wishtony <vishnutoanother@gmail.com>
2024-01-30 17:32:44 +0530

    commit

--- src/components/Center.js
+++ src/components/Center.js
@@ -6,13 +6,14 @@ import "../styles/header.css";
 
 function Center() {
   return (
-    <div className="center-div mt-5" style={{marginTop:"22rem"}}>
+    <div className="center-div mt-5" style={{ marginTop: "22rem" }}>
       <div className="three-div row gx-lg-5 gx-md-4 gx-sm-3">
         <div className="col-lg-4 col-md-6 mb-lg-3 mb-md-3 mb-sm-3">
           <div className="d-flex flex-column">
             <img src={mask1} alt=""></img>
             <div>
-              Explore large, destructible environments where no two games are ever the same.
+              Explore large, destructible environments where no two games are
+              ever the same.
             </div>
           </div>
         </div>
@@ -21,7 +22,8 @@ function Center() {
           <div className="d-flex flex-column">
             <img src={mask2} alt=""></img>
             <div>
-              Team up with friends by sprinting, climbing, and smashing your way to earn your Victory Royale
+              Team up with friends by sprinting, climbing, and smashing your way
+              to earn your Victory Royale
             </div>
           </div>
         </div>
@@ -30,7 +32,8 @@ function Center() {
           <div className="d-flex flex-column">
             <img src={mask3} alt=""></img>
             <div>
-              Discover even more ways to play across thousands of creator-made game genres
+              Discover even more ways to play across thousands of creator-made
+              game genres
             </div>
           </div>
         </div>
